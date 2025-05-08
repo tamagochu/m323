e83befaebdbacc0d8e48d375bd0b274d59532a67
Jessica Fürst <jvfuerst01@gmail.com>
2025-05-08 11:49:58 +0200

    Update HighScore.java

--- miniprojekt/HighScorelist/src/main/java/HighScore.java
+++ miniprojekt/HighScorelist/src/main/java/HighScore.java
@@ -1,8 +1,10 @@
-class HighScore {
-    String name;
-    String date;
-    String level;
-    int time;
+import java.util.Objects;
+
+final class HighScore {
+    final String name;
+    final String date;
+    final String level;
+    final int time;
 
     public HighScore(String name, String date, String level, int time) {
         this.name = name;
@@ -13,6 +15,20 @@ class HighScore {
 
     @Override
     public String toString() {
-        return name + " (" + date + ") - " + level + ": " + time + " sec";
+        return name + " - " + time + "s on " + date;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof HighScore other)) return false;
+        return name.equals(other.name)
+                && date.equals(other.date)
+                && level.equals(other.level)
+                && time == other.time;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, date, level, time);
     }
 }
